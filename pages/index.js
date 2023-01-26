7267f65f144f395394ec0d92ac41503396e8b9e5
Garam-Leee <grlee0806@gmail.com>
2023-01-26 16:28:39 +0900

    @emotion/styled 적용

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import styled from '@emotion/styled';
 
 export default function Home() {
-  return (
-    <>
-      <div>sd</div>
-    </>
-  );
+  return <Container>styled componet</Container>;
 }
+
+const Container = styled.section`
+  background-color: var(--color-purple10);
+  color: var(--color-white);
+  overflow-y: hidden;
+  padding: 5px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  height: 100vh;
+`;
