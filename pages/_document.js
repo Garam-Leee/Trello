22e4795814966c1dabf3e8fd4bdbc5888b8ca5ce
Garam-Leee <grlee0806@gmail.com>
2023-01-26 16:22:09 +0900

    reset.css 적용

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>HI THERE</title>
+          <meta charSet='utf-8'></meta>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Head>
+      </Html>
+    );
+  }
+}
